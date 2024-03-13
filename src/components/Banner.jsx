5acf5e1d8396a1069a4521f0ef9ba55deabb81ef
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2024-03-13 14:31:41 +0500

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,47 +2,51 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className=" pb-4  font-sans bg-black text-white">
+    // Banner
+    <div class="pb-4 font-sans bg-black text-white">
       {/* Intro */}
-      <div className="px-4 sm:px-8 lg:px-20 xl:px-28">
-        <h1 className="font-bold text-3xl md:text-3xl lg:text-4xl xl:text-5xl flex flex-wrap">
-          <span className="text-primary">#1WEB</span>
+      <div class="px-4 sm:px-8 lg:px-20 xl:px-28">
+        <h1 class="font-bold text-3xl md:text-3xl lg:text-4xl xl:text-5xl flex flex-wrap">
+          <span class="text-primary">#1WEB </span>
           DEVELOPMENT COMPANY
         </h1>
-        <p className="text-xs py-4">
+        <p class="text-xs py-4">
           Transformative builders of innovation. We shape technology into
           revolutionary web experiences, fueled by your unique vision.
         </p>
-        <button className="bg-primary rounded-md py-1 px-12">LET'S TALK</button>
+        <button class="bg-primary rounded-md py-1 px-8 sm:px-12">
+          LET'S TALK
+        </button>
       </div>
+
       {/* Form */}
-      <div className="bg-white p-5 py-6 rounded-xl text-black my-8 flex flex-cols justify-center items-center mx-4 sm:mx-8 lg:mx-20 xl:mx-28">
-        <form className="flex flex-col justify-center items-center">
-          <h2 className="font-semibold text-md text-center mx-auto">
+      <div class="bg-white p-5 py-6 rounded-xl text-black my-8 mx-4 sm:mx-8 lg:mx-20 xl:mx-28">
+        <form class="flex flex-col justify-center items-center">
+          <h2 class="font-semibold text-md text-center mx-auto mb-4">
             Let's Start a Project Together
           </h2>
-          <div className="bg-primary text-primary h-0.5 w-48 my-4"></div>
+          <div class="bg-primary text-primary h-0.5 w-44 sm:w-32 lg:w-48 my-2 mx-auto"></div>
           <input
-            className="p-2 pl-0 w-48  pr-0 mb-2 border-b border-gray-500 text-xs text-left"
+            class="p-2 pl-0 w-full mb-2 border-b border-gray-500 text-xs text-left"
             type="text"
             placeholder="Full Name"
           />
           <input
-            className="p-2 pl-0  w-48 pr-0 mb-2 border-b border-gray-500 text-xs text-left"
+            class="p-2 pl-0 w-full mb-2 border-b border-gray-500 text-xs text-left"
             type="text"
             placeholder="Email"
           />
           <input
-            className="p-2 pl-0 w-48 pr-0 mb-2 border-b border-gray-500 text-xs text-left"
+            class="p-2 pl-0 w-full mb-2 border-b border-gray-500 text-xs text-left"
             type="text"
             placeholder="Number"
           />
           <input
-            className="p-2 pl-0 w-48 pr-0 mb-2 border-b border-gray-500 text-xs text-left"
+            class="p-2 pl-0 w-full mb-2 border-b border-gray-500 text-xs text-left"
             type="text"
             placeholder="Describe Your Project Need."
           />
-          <button className="text-white bg-primary rounded-md py-1.5 w-48 mt-5 mb-1">
+          <button class="text-white bg-primary rounded-md py-1.5 w-full mt-5 mb-1">
             Let's Talk
           </button>
         </form>
