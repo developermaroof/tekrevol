2f27ab6f860d6d1ab5405bc37460feb3304e395a
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2024-04-09 17:09:12 +0500

    Updated

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import ClientSlider from "../Sliders/ClientSlider";
+import "../Stylings/client.css";
 
 const Clients = () => {
   return (
     <div className="w-full h-full p-4">
       <div>
-        <h1 className="py-4 text-3xl text-center font-bold">
+        <h1 className="py-4 text-3xl text-center font-bold md:text-4xl md:pb-20 lg:text-5xl lg:text-gray-800">
           Our Clients Love Our Work!
         </h1>
       </div>
       <ClientSlider>
         {/* 1st Client */}
-        <div>
+        <div className="md:flex md:gap-10">
           <div>
-            <h2 className="font-bold text-xl py-2 pt-4">Cristina Soomro</h2>
-            <span className="text-gray-400 font-semibold text-md">
+            <h2 className="font-bold text-xl py-2 pt-4 md:text-2xl lg:text-3xl lg:text-gray-800">
+              Cristina Soomro
+            </h2>
+            <span className="text-gray-400 font-semibold text-md lg:text-lg">
               The Veteran App, Co-Founder
             </span>
-            <p className="py-4 text-sm">
+            <p className="py-4 text-sm lg:text-lg">
               “Tekrevol has been amazing…it’s a very big project so they break
               it down into chunks and just perform. I can’t recommend them
               enough.”
@@ -33,18 +36,20 @@ const Clients = () => {
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerpolicy="strict-origin-when-cross-origin"
               allowfullscreen
-              style={{ borderRadius: "10px", width: "100%", height: "100%" }}
+              className="iframe"
             ></iframe>
           </div>
         </div>
         {/* 2nd Client */}
-        <div>
+        <div className="md:flex md:gap-10">
           <div>
-            <h2 className="font-bold text-xl py-2 pt-4">Micheal Baroch</h2>
-            <span className="text-gray-400 font-semibold text-md">
+            <h2 className="font-bold text-xl py-2 pt-4 md:text-2xl lg:text-3xl lg:text-gray-800">
+              Micheal Baroch
+            </h2>
+            <span className="text-gray-400 font-semibold text-md lg:text-lg">
               Oz Leasing, Owner
             </span>
-            <p className="py-4 text-sm">
+            <p className="py-4 text-sm lg:text-lg">
               “Tekrevol has been amazing…it’s a very big project so they break
               it down into chunks and just perform. I can’t recommend them
               enough.”
@@ -60,18 +65,21 @@ const Clients = () => {
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerpolicy="strict-origin-when-cross-origin"
               allowfullscreen
-              style={{ borderRadius: "10px", width: "100%", height: "100%" }}
+              className="iframe"
             ></iframe>
           </div>
         </div>
         {/* 3rd Client */}
-        <div>
+        <div className="md:flex md:gap-10">
           <div>
-            <h2 className="font-bold text-xl py-2 pt-4"> Bob Lagari</h2>
-            <span className="text-gray-400 font-semibold text-md">
+            <h2 className="font-bold text-xl py-2 pt-4 md:text-2xl lg:text-3xl lg:text-gray-800">
+              {" "}
+              Bob Lagari
+            </h2>
+            <span className="text-gray-400 font-semibold text-md lg:text-lg">
               TalPool Holdings, Founder
             </span>
-            <p className="py-4 text-sm">
+            <p className="py-4 text-sm lg:text-lg">
               “Tekrevol has been amazing…it’s a very big project so they break
               it down into chunks and just perform. I can’t recommend them
               enough.”
@@ -87,7 +95,7 @@ const Clients = () => {
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerpolicy="strict-origin-when-cross-origin"
               allowfullscreen
-              style={{ borderRadius: "10px", width: "100%", height: "100%" }}
+              className="iframe"
             ></iframe>
           </div>
         </div>
