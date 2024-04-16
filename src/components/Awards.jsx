e2b031ecd692af602c269fb1acc30754237e91ca
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2024-04-16 14:55:00 +0500

    Updated

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import AwardSlider from "../Sliders/AwardSlider";
+import Clutch from "../assets/awards/Clutch.webp";
+import Dot from "../assets/awards/dot-com.webp";
+import Expertise from "../assets/awards/expertise-logo.webp";
+import HorizonSilver from "../assets/awards/Horizon_Sliver.webp";
+import Horizon from "../assets/awards/Horizon.webp";
+import Inc from "../assets/awards/inc-5000.webp";
+import Rank from "../assets/awards/rank-watch.webp";
+import Right from "../assets/awards/right-firms.webp";
+import TopDev from "../assets/awards/top_developer.webp";
+import TopRated from "../assets/awards/top_Reated.webp";
+import Web from "../assets/awards/web-excellence-award.webp";
 
 const Awards = () => {
   return (
     <div>
       <AwardSlider>
         <div className="flex justify-center items-center text-center flex-col text-xs">
-          <img
-            src="https://www.tekrevol.com/static/1376e1fe11a32117b4ac5f51334ed0d2/4cd7f/Horizon.webp"
-            alt=""
-            className="w-44 h-auto"
-          />
+          <img src={Horizon} alt="" className="w-44 h-auto" />
           <h1 className="text-lg  text-gray-700 font-semibold">
             Horizon Award
           </h1>
@@ -18,94 +25,54 @@ const Awards = () => {
           <p>Gold Awards Winner 2020</p>
         </div>
         <div className="flex justify-center items-center text-center flex-col text-xs">
-          <img
-            src="https://www.tekrevol.com/static/363029a2b76f79f9335a73fd6ab915e9/4cd7f/dot-com.webp"
-            alt=""
-            className="w-44 h-auto"
-          />
+          <img src={Dot} alt="" className="w-44 h-auto" />
           <h1 className="text-lg  text-gray-700 font-semibold">Dot Comm</h1>
           <p>Excellence in Web Creativity &amp; Digital Communication</p>
         </div>
         <div className="flex justify-center items-center text-center flex-col text-xs">
-          <img
-            src="https://www.tekrevol.com/static/fbf1158a283aabd029616a75f9d999bd/4cd7f/inc-5000.webp"
-            alt=""
-            className="w-44 h-auto"
-          />
+          <img src={Inc} alt="" className="w-44 h-auto" />
           <h1 className="text-lg text-gray-700 font-semibold">INC. 5000</h1>
           <p>America's Fastest Growing Companies</p>
         </div>
         <div className="flex justify-center items-center text-center flex-col text-xs">
-          <img
-            src="https://www.tekrevol.com/static/63ccecab4faa1fc941650a049be0dda9/4cd7f/rank-watch.webp"
-            alt=""
-            className="w-44 h-auto"
-          />
+          <img src={Rank} alt="" className="w-44 h-auto" />
           <h1 className="text-lg text-gray-700 font-semibold">Rank Watch</h1>
           <p>Top Web Development Agencies 2023</p>
         </div>
         <div className="flex justify-center items-center text-center flex-col text-xs">
-          <img
-            src="https://www.tekrevol.com/static/0a8481d5afc4147f9c9e7ca4c5fafb0f/4cd7f/Horizon_Sliver.webp"
-            alt=""
-            className="w-44 h-auto"
-          />
+          <img src={HorizonSilver} alt="" className="w-44 h-auto" />
           <h1 className="text-lg text-gray-700 font-semibold">Horizon Award</h1>
           <p>Silver Awards Winner 2020</p>
         </div>
         <div className="flex justify-center items-center text-center flex-col text-xs">
-          <img
-            src="https://www.tekrevol.com/static/1bac00d1ad0d571f07951d583a3c2af7/4cd7f/Clutch.webp"
-            alt=""
-            className="w-44 h-auto"
-          />
+          <img src={Clutch} alt="" className="w-44 h-auto" />
           <h1 className="text-lg text-gray-700 font-semibold">Clutch</h1>
           <p>Top 1000 Companies</p>
         </div>
         <div className="flex justify-center items-center text-center flex-col text-xs">
-          <img
-            src="https://www.tekrevol.com/static/97f5963fa8f15547d65344e2b851c15a/4cd7f/right-firms.webp"
-            alt=""
-            className="w-44 h-auto"
-          />
+          <img src={Right} alt="" className="w-44 h-auto" />
           <h1 className="text-lg text-gray-700 font-semibold">Right firms</h1>
           <p>Top Mobile App Development Company 2023</p>
         </div>
         <div className="flex justify-center items-center text-center flex-col text-xs">
-          <img
-            src="https://www.tekrevol.com/static/2a16fccc8a9da223971db5ae90ce749f/4cd7f/top_developer.webp"
-            alt=""
-            className="w-44 h-auto"
-          />
+          <img src={TopDev} alt="" className="w-44 h-auto" />
           <h1 className="text-lg text-gray-700 font-semibold">
             Top Entertainment
           </h1>
           <p>Top Entertainment App Developers</p>
         </div>
         <div className="flex justify-center items-center text-center flex-col text-xs">
-          <img
-            src="https://www.tekrevol.com/static/fd1f077a02d3a0e8fca0a24ba34eeb2d/4cd7f/expertise-logo.webp"
-            alt=""
-            className="w-44 h-auto"
-          />
+          <img src={Expertise} alt="" className="w-44 h-auto" />
           <h1 className="text-lg text-gray-700 font-semibold">Expertise</h1>
           <p>Best Mobile App Developer</p>
         </div>
         <div className="flex justify-center items-center text-center flex-col text-xs">
-          <img
-            src="https://www.tekrevol.com/static/bddaf76293d5cbd6e3c6dfa337694e0b/4cd7f/top_Reated.webp"
-            alt=""
-            className="w-44 h-auto"
-          />
+          <img src={TopRated} alt="" className="w-44 h-auto" />
           <h1 className="text-lg text-gray-700 font-semibold">Top Rated</h1>
           <p>Top App Development Companies</p>
         </div>
         <div className="flex justify-center items-center text-center flex-col text-xs">
-          <img
-            src="https://www.tekrevol.com/static/d946b7548af9ae21c6aa8ee07414154a/4cd7f/web-excellence-award.webp"
-            alt=""
-            className="w-44 h-auto"
-          />
+          <img src={Web} alt="" className="w-44 h-auto" />
           <h1 className="text-lg text-gray-700 font-semibold">
             Web Excellence
           </h1>
